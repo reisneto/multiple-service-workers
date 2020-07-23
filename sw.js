a13962ce555d25a649f6e4cbf8251275913de5f3
Alvaro Reis <alvaro.neto@linx3.com>
2020-07-23 15:40:45 -0400

    feat: add cache to local sw

--- sw.js
+++ sw.js
@@ -1,3 +1,35 @@
+var CACHE_NAME = 'all-cache-v1';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles/main.css',
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
 self.addEventListener('push', function(e) {
     var options = {
       body: 'This notification was generated from a push - great!',
