541c9bbf3390b517d54b4df51b46e387929fe697
Alvaro Reis <alvaro.neto@linx3.com>
2020-07-23 10:24:37 -0400

    feat: add image to notification

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
 self.addEventListener('push', function(e) {
     var options = {
-      body: 'This notification was generated from a push!',
-      icon: 'images/example.png',
+      body: 'This notification was generated from a push - great!',
+      icon: 'images/yaymeme.jpeg',
       vibrate: [100, 50, 100],
       data: {
         dateOfArrival: Date.now(),
         primaryKey: '2'
       },
       actions: [
-        {action: 'explore', title: 'Explore this new world',
+        {action: 'more', title: 'Learn more',
           icon: 'images/checkmark.png'},
         {action: 'close', title: 'Close',
           icon: 'images/xmark.png'},
       ]
     };
     e.waitUntil(
-      self.registration.showNotification('Hello world!', options)
+      self.registration.showNotification(`Hi, it's a test!`, options)
     );
   });
